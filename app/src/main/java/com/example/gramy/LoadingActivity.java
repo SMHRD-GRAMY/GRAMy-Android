667eada04b36aeb1dcefe95b756255040204828d
yeonghyeJo <0808yeonghye@gmail.com>
2022-04-30 15:17:05 +0900

    [영혜] 로그인화면 uiux 설계

--- app/src/main/java/com/example/gramy/LoadingActivity.java
+++ app/src/main/java/com/example/gramy/LoadingActivity.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TestActivity extends AppCompatActivity {
+public class LoadingActivity extends AppCompatActivity {
 
+    // 로딩화면입니다. 로고만 일단 ImageView로 배치해놨습니다.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.header_footer);
+        setContentView(R.layout.activity_loading);
     }
 }
--- app/src/main/java/com/example/gramy/TutorialActivity.java
+++ app/src/main/java/com/example/gramy/TutorialActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TestActivity extends AppCompatActivity {
+public class TutorialActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.header_footer);
+        setContentView(R.layout.activity_tutorial);
     }
 }
