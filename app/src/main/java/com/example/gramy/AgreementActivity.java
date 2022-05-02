442ae08bdf43dc8477d3855d3c5c165b37d1a93d
조영혜 <0808yeonghye@gmail.com>
2022-05-02 12:11:41 +0900

    [영혜] 회원가입 전 이용약관 동의 페이지 기능 구현완료. 약관의 체크박스는 동그랗게 수정 예정.

--- app/src/main/java/com/example/gramy/AgreementActivity.java
+++ app/src/main/java/com/example/gramy/AgreementActivity.java
@@ -2,13 +2,89 @@ package com.example.gramy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
+// 이용약관 체크박스 기능구현단입니다.
 public class AgreementActivity extends AppCompatActivity {
+
+    // 라디오 버튼 옆에 해당 이용약관 전문 볼 수 있는 용도의 버튼은 만들어놨는데 이 부분은 추후에 기능 구현 진행하겠습니다.
+    CheckBox ckAll, ckAgeNo, ckAgeYes, ckGramy, ckPersonalChoose, ckMarketing, ckPersonal;
+    Button btnNext;
+    boolean isChecked = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agreement);
+
+        ckAll = findViewById(R.id.ckAll);
+        ckAgeNo = findViewById(R.id.ckAgeNo);
+        ckAgeYes = findViewById(R.id.ckAgeYes);
+        ckGramy = findViewById(R.id.ckGramy);
+        ckPersonal = findViewById(R.id.ckPersonal);
+        ckPersonalChoose = findViewById(R.id.ckPersonalChoose);
+        ckMarketing = findViewById(R.id.ckMarketing);
+        btnNext = findViewById(R.id.btnNext);
+
+        ckAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ckAll.isChecked()) {
+                    ckGramy.setChecked(true);
+                    ckPersonal.setChecked(true);
+                    ckPersonalChoose.setChecked(true);
+                    ckMarketing.setChecked(true);
+                }else{
+                    ckGramy.setChecked(false);
+                    ckPersonal.setChecked(false);
+                    ckPersonalChoose.setChecked(false);
+                    ckMarketing.setChecked(false);
+                }
+            }
+        });
+        ckAgeNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ckAgeNo.isChecked()){
+                    ckAgeNo.setChecked(true);
+                    ckAgeYes.setChecked(false);
+                }else{
+                    ckAgeYes.setChecked(false);
+                    ckAgeNo.setChecked(true);
+                }
+            }
+        });
+        ckAgeYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ckAgeYes.isChecked()){
+                    if (ckAgeNo.isChecked()){
+                        ckAgeNo.setChecked(false);
+                        ckAgeYes.setChecked(true);
+                    }else{
+                        ckAgeYes.setChecked(true);
+                        ckAgeNo.setChecked(false);
+                    }
+                }
+            }
+        });
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ckGramy.isChecked()==true && ckPersonal.isChecked()==true && ckAgeYes.isChecked()==true) {
+                    Intent intent = new Intent(AgreementActivity.this, JoinActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
