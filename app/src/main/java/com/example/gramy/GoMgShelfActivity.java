1c904af5ff140fd48ff1b900f73b56ffab872cd6
조영혜 <0808yeonghye@gmail.com>
2022-05-06 13:50:01 +0900

    [영혜] 홈화면 버튼 기능 구현. 프래그먼트 안에서 진행해야 해서 메소드 추가생성.

---
+++ app/src/main/java/com/example/gramy/GoMgShelfActivity.java
@@ -0,0 +1,13 @@
+package com.example.gramy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GoMgShelfActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_go_mg_shelf);
+    }
+}
---
+++ app/src/main/java/com/example/gramy/GoReportActivity.java
@@ -0,0 +1,14 @@
+package com.example.gramy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_go_report);
+    }
+}
