2bc94d13194720e32f97179432e3a8e95b6f79a9
Jong jae Park <66498240+pjj186@users.noreply.github.com>
2022-05-10 20:26:39 +0900

    [종재] - News

--- app/src/main/java/com/example/gramy/news/fragNewsmain.java
+++ app/src/main/java/com/example/gramy/news/fragNewsmain.java
@@ -1,26 +1,18 @@
 package com.example.gramy.news;
 
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.os.Handler;
-import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.gramy.R;
 
@@ -30,91 +22,104 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class fragNewsmain extends Fragment {
 
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+
+    private String mParam1;
+    private String mParam2;
+
     private ListView listView;
-    String rssURL = "https://fs.jtbc.joins.com//RSS/newsflash.xml";
 
-
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-
-
-        View rootView = inflater.inflate(R.layout.fragment_frag_newsmain, container, false);
-
-
-        listView = rootView.findViewById(R.id.NewsList);
-
-
-
-        NewsTask task=new NewsTask();
-        task.execute(rssURL);
-
-        return rootView;
+    public fragNewsmain() {
+        // Required empty public constructor
     }
 
-    class NewsTask extends AsyncTask<String,Void,Void>{
 
+    public static fragNewsmain newInstance(String param1, String param2) {
+        fragNewsmain fragment = new fragNewsmain();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_frag_newsmain, container, false);
+
+        listView = view.findViewById(R.id.NewsList);
+
+        new NewsTask().execute();
+
+        return view;
+    }
+
+    class NewsTask extends AsyncTask<Void, Void, Void> {
         List<NewsVO> newsList;
 
-        //실제작업
         @Override
-        protected Void doInBackground(String... params) {
-
+        protected Void doInBackground(Void... voids) {
 
             try {
-                Document document = Jsoup.connect(rssURL).get();
+                Document document = Jsoup.connect("https://fs.jtbc.joins.com//RSS/newsflash.xml").get();
                 Elements elements = document.select("item");
-                Log.d("뉴스 다 읽었는지 찍어볼게요",elements.toString());
                 for (Element element : elements) {
                     NewsVO data = new NewsVO();
 
                     Elements e = element.select("title");
-                    if (e != null && e.size()>0)
+                    if (e != null && e.size() > 0)
                         data.setTitle(e.get(0).text());
 
                     e = element.select("link");
-                    if (e != null && e.size()>0)
+                    if (e != null && e.size() > 0)
                         data.setLink(e.get(0).text());
 
                     e = element.select("description");
-                    if (e != null && e.size()>0)
+                    if (e != null && e.size() > 0)
                         data.setDescription(e.get(0).text());
 
                     e = element.select("pubDate");
-                    if (e != null && e.size()>0)
+                    if (e != null && e.size() > 0)
                         data.setPubDate(e.get(0).text());
 
                     newsList.add(data);
                 }
-                Log.d("뉴스 다 읽었는지 찍어볼게요",newsList.toString());
+                Log.d("뉴스 다 읽었는지 찍어볼게요", newsList.toString());
             } catch (IOException e) {
                 e.printStackTrace();
             }
             return null;
         }
 
-        //가장 먼저실행
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
             newsList = new ArrayList<>();
         }
 
-
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-            NewsAdapter adapter = new NewsAdapter(getContext(),newsList);
+            NewsAdapter adapter = new NewsAdapter(getContext(), newsList);
             listView.setAdapter(adapter);
             listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
@@ -124,10 +129,5 @@ public class fragNewsmain extends Fragment {
                 }
             });
         }
-
-
     }
-
-
-
 }
