c6ef4c1695d30696e64ae66df5ff25272e01ae4c
Jong jae Park <pjj186@naver.com>
2022-05-14 11:04:30 +0900

    [종재] 게시물 작성 화면 구성 (아직 로직 X)

--- app/src/main/java/com/example/gramy/BoardWriteActivity.java
+++ app/src/main/java/com/example/gramy/BoardWriteActivity.java
@@ -2,13 +2,44 @@ package com.example.gramy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class BoardWriteActivity extends AppCompatActivity {
+
+    TextView boardWriteBack;
+    Button btnWrtieCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_write);
+
+        boardWriteBack = findViewById(R.id.boardWriteBack);
+        btnWrtieCancel = findViewById(R.id.btnWriteCancel);
+
+        // 게시글 작성 취소
+        btnWrtieCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), GoBoardActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        // 게시글 목록으로 이동
+        boardWriteBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), GoBoardActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
